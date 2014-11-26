e7cecd8d9ca85738c9a5755705244a14dc0223df
Franz Andrew Vallente <franzandrew68@gmail.com>
2014-11-26 21:44:02 +0800

    Add red and yellow warning support

--- includes/js/script.js
+++ includes/js/script.js
@@ -37,10 +37,41 @@ function format(n) {
 	return (n < 10 ? "0" : "") + n;
 }
 
+function yellow() {
+	var interval = setInterval(function() {
+		$("body").css('background', 'yellow');
+		setTimeout("$('body').css('background', '')", 500);
+	}, 1000);
+	return interval;
+}
+
+function red() {
+	var interval = setInterval(function() {
+		$("body").css('background', 'red');
+		setTimeout("$('body').css('background', '')", 500);
+	}, 1000);
+	return interval;
+}
+
 var counter = new Timer({
 	minutes: 3,
 	seconds: 0,
 	onUpdateStatus: function(min, sec){
+		
+		var secondsleft = min*60 + sec;
+		
+		var yellowwarning = 2*60 + 0;
+		var redwarning = 1*60 + 0; 
+		
+		if(secondsleft == yellowwarning) {
+			yellow();
+		}
+		
+		if(secondsleft == redwarning) {
+			clearInterval(yellow());
+			red();
+		}
+		
 		$("#minute").html(format(min));
 		$("#second").html(format(sec));
 	}
